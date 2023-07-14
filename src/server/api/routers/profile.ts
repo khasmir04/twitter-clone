a4882601d6b731d34a2189be9b5f37093063df78
khasmir04 <kcaluscusao@gmail.com>
2023-07-14 10:01:40 +0800

    feat: ssg revalidation logic

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -67,7 +67,8 @@ export const profileRouter = createTRPCRouter({
         addedFollow = false
       }
 
-      // TODO: Revalidation
+      void ctx.revalidateSSG?.(`/profiles/${userId}`)
+      void ctx.revalidateSSG?.(`/profiles/${currentUserId}`)
 
       return { addedFollow }
     }),
